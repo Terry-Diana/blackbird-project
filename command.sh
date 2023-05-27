60ce0a7d5045574294af8c5bc6cdf6d29298bd88
Terry-Diana <terry.wdiana@gmail.com>
2023-05-27 12:00:10 +0300

    add command.sh

---
+++ command.sh
@@ -0,0 +1,20 @@
+1. npx create-react-app blackbird
+2. git init
+   git add .
+   git commit -m "first-commit"
+   git branch -M main
+   git remote add origin https://github.com/Terry-Diana/blackbird-project.git
+   git push -u origin main
+3. git branch upload_logo
+   git checkout upload_logo
+4. Replace existing logo with https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg
+5. Replace existing link with https://www.propelleraero.com/dirtmate/
+6. git add .
+   git commit -m "add logo and link"
+   git push
+7. Create PR from “update_logo” to “master” from github CLI
+8. Merge the PR using GitHub CLI
+
+# REPO_URL https://github.com/Terry-Diana/blackbird-project.git
+
+
